a2d33074151ca8a440edd0b7208e29846dc35c56
prbn97 <pauloreis.rj97@gmail.com>
2024-04-11 13:03:05 -0300

    21 solved

--- 21_merge_two_sorted_lists/main.go
+++ 21_merge_two_sorted_lists/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // https://leetcode.com/problems/merge-two-sorted-lists/
 func main() {
 	list1 := &ListNode{Val: 1}
@@ -22,16 +24,42 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// list1 = [1,2,4]
+// list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	currentNodeL1 := list1
+	// Criação de um nó cabeça para a lista resultante
+	head := &ListNode{}
+	current := head
 
-	for list2 != nil {
-		// if currentNodeL1.Val <= list2.Val {
-		// currentNodeL1.Next = list2
-		// }
-
+	// Percorre ambas as listas
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		// Avançar para o próximo nó na lista resultante
+		current = current.Next
 	}
 
-	return list1
+	// Se uma das listas chegou ao fim, mas a outra não
+	// Adiciona o restante dos nós da lista não vazia à lista resultante
+	if list1 != nil {
+		current.Next = list1
+	}
+	if list2 != nil {
+		current.Next = list2
+	}
 
+	for head.Next != nil {
+		fmt.Println(head.Next.Val)
+		head.Next = head.Next.Next
+
+	}
+	// Retornar a lista resultante sem o nó cabeça
+	return head.Next
 }
