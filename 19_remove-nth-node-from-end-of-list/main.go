2380d5c6cd1deb52aea167f3ae9a1d4bbd0af665
prbn97 <pauloreis.rj97@gmail.com>
2024-04-19 14:21:03 -0300

    solving question 4

--- 19_remove-nth-node-from-end-of-list/main.go
+++ 19_remove-nth-node-from-end-of-list/main.go
@@ -1,24 +1,52 @@
 package main
 
-type headNode struct {
+type ListNode struct {
 	Val  int
-	Next *headNode
+	Next *ListNode
 }
 
 func main() {
 	// Input: head = [1,2,3,4,5], n = 2
-	head := &headNode{Val: 1}
-	head.Next = &headNode{Val: 2}
-	head.Next.Next = &headNode{Val: 3}
-	head.Next.Next.Next = &headNode{Val: 4}
-	head.Next.Next.Next.Next = &headNode{Val: 5}
-	removeNthFromEnd(head, 2)
-	// Output: [1,2,3,5]
+	var head *ListNode
+
+	for i := 5; i >= 1; i-- {
+		node := ListNode{
+			Val:  i,
+			Next: head,
+		}
+
+		head = &node
+	}
 
 }
 
-// https://leetcode.com/problems/remove-nth-node-from-end-of-head/description/
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
-func removeNthFromEnd(head *headNode, n int) *headNode {
+// removeNthFromEnd(head, 2)
+// Output: [1,2,3,5]
 
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	// Criação de um nó cabeça para facilitar a remoção do primeiro elemento
+	dummy := &ListNode{Next: head}
+
+	// Inicializar dois ponteiros, leader e follower
+	leader := dummy
+	follower := dummy
+
+	// Avançar o ponteiro leader por n+1 nós
+	for i := 0; i <= n; i++ {
+		leader = leader.Next
+	}
+
+	// Avançar ambos os ponteiros até que o líder chegue ao final da lista
+	for leader != nil {
+		leader = leader.Next
+		follower = follower.Next
+	}
+
+	// Remover o nó referenciado pelo ponteiro follower
+	follower.Next = follower.Next.Next
+
+	// Retornar o nó cabeça da lista (dummy.Next)
+	return dummy.Next
 }
--- 4_median-of-two-sorted-arrays/main.go
+++ 4_median-of-two-sorted-arrays/main.go
@@ -3,4 +3,23 @@ package main
 
 func main() {
 
+	nums1, nums2 := []int{1, 3}, []int{2}
+}
+
+// create binary tree
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// create func insert(root *TreeNode, val int) *TreeNode
+// iterarei the arrays and use the func insert
+// find the median number in float64 and return
+// if the tree have number even of nodes, the median is the average of the two middle numbers.
+// if odd number of nodes is the val in the middle
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+
+	return 2.0
 }
