52d1f1fed0ec3b0b4e422693e951ad451e57c814
prbn97 <pauloreis.rj97@gmail.com>
2024-06-17 13:48:25 -0300

    exercie 4 changes

--- 4_median-of-two-sorted-arrays/main.go
+++ 4_median-of-two-sorted-arrays/main.go
@@ -3,7 +3,9 @@ package main
 
 func main() {
 
-	nums1, nums2 := []int{1, 3}, []int{2}
+	nums1, nums2 := []int{1, 3}, []int{2} //2
+
+	nums3, nums4 := []int{1, 2}, []int{3, 4} //2.5
 }
 
 // create binary tree
@@ -17,7 +19,7 @@ type TreeNode struct {
 // iterarei the arrays and use the func insert
 // find the median number in float64 and return
 // if the tree have number even of nodes, the median is the average of the two middle numbers.
-// if odd number of nodes is the val in the middle
+// if odd number of nodes is the val in the middle"
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
