46fae115f28711df3b9b371d0f85d98bf2a2a91c
prbn97 <pauloreis.rj97@gmail.com>
2024-10-11 00:43:40 -0300

    dive into linked list 8

--- 1.Linked Lists/3.LinkedList.js
+++ 1.Linked Lists/3.LinkedList.js
@@ -1,10 +1,10 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
- 
+
 class LinkedList {
     constructor(value) {
         const newNode = new Node(value);
@@ -12,41 +12,6 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
-
-    printList() {
-        let temp = this.head;
-        while (temp !== null) {
-            console.log(temp.value);
-            temp = temp.next;
-        }
-    }
-
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
-        } else {
-            console.log("Head: " + this.head.value);
-        }
-    }
-
-    getTail() {
-        if (this.tail === null) {
-            console.log("Tail: null");
-        } else {
-            console.log("Tail: " + this.tail.value);
-        }
-    }
-
-    getLength() {
-        console.log("Length: " + this.length);
-    }
-
-    makeEmpty() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
- 
     push(value) {
         const newNode = new Node(value);
         if (!this.head) {
@@ -59,9 +24,8 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
     pop() {
-        if (this.length === 0) return undefined;
+        if (!this.head) return undefined;
         let temp = this.head;
         let pre = this.head;
         while (temp.next) {
@@ -77,7 +41,6 @@ class LinkedList {
         }
         return temp;
     }
- 
     unshift(value) {
         const newNode = new Node(value);
         if (!this.head) {
@@ -90,9 +53,8 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
     shift() {
-        if (this.length === 0) return undefined;
+        if (!this.head) return undefined;
         let temp = this.head;
         this.head = this.head.next;
         this.length--;
@@ -102,7 +64,6 @@ class LinkedList {
         temp.next = null;
         return temp;
     }
- 
     get(index) {
         if (index < 0 || index >= this.length) return undefined;
         let temp = this.head;
@@ -111,7 +72,6 @@ class LinkedList {
         }
         return temp;
     }
- 
     set(index, value) {
         let temp = this.get(index);
         if (temp) {
@@ -120,12 +80,11 @@ class LinkedList {
         }
         return false;
     }
- 
     insert(index, value) {
         if (index < 0 || index > this.length) return false;
         if (index === this.length) return this.push(value);
         if (index === 0) return this.unshift(value);
-        
+
         const newNode = new Node(value);
         const temp = this.get(index - 1);
         newNode.next = temp.next;
@@ -133,62 +92,22 @@ class LinkedList {
         this.length++;
         return true;
     }
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
 
+        const before = this.get(index - 1);
+        const temp = before.next;
+
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
 }
 
-
-function test() {
-
-    let myLinkedList = new LinkedList(1);
-    myLinkedList.push(3);
-
-    console.log("LL before insert():");
-    myLinkedList.printList();
-
-    myLinkedList.insert(1, 2);
-
-    console.log("\nLL after insert(2) in middle:");
-    myLinkedList.printList();
-
-    myLinkedList.insert(0, 0);
-
-    console.log("\nLL after insert(0) at beginning:");
-    myLinkedList.printList();
-
-    myLinkedList.insert(4, 4);
-
-    console.log("\nLL after insert(4) at end:");
-    myLinkedList.printList();
-
-}
-
-
-test();
-
-
-/*
-    EXPECTED OUTPUT:
-    ----------------
-    LL before insert():
-    1
-    3
-
-    LL after insert(2) in middle:
-    1
-    2
-    3
-
-    LL after insert(0) at beginning:
-    0
-    1
-    2
-    3
-
-    LL after insert(4) at end:
-    0
-    1
-    2
-    3
-    4
-
-*/
+let linkedList = new LinkedList(11);
+linkedList.push(3);
+linkedList.push(23);
+linkedList.push(7);
